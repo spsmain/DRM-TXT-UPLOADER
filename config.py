27953882beb785024e8e3106542c9c231a057ad9
spsmain <129521948+spsmain@users.noreply.github.com>
2025-05-21 05:54:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 API_ID = os.environ.get("API_ID", "25033617") #Replace With your api id
 API_HASH = os.environ.get("API_HASH", "4c6d9c4757c0c253c680e284b17d7f54") #Replace With your api hash
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7647566125:AAGauuiZqOSiOvDmMFQbEW865vPA0qOg6Z4") #Replace With your bot token
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") #Replace With your bot token
